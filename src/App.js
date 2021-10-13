a7c12f4b594bceb5372bb8957f909c81288a45e0
Malcolm Lima <malcolmmellolima@gmail.com>
2021-10-13 06:31:54 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Menu, PageBody } from './AppStyled';
+
+import HomeScreen from './pages/HomeScreen';
+import Tela2Screen from './pages/Tela2Screen';
+
+import MenuItem from './components/MenuItem'
+
+export default () => {
+    const name = useSelector(state => state.user.name);
+
+    return (
+        <BrowserRouter>
+            <Container>
+                <Menu>
+                    <MenuItem icon="/assets/store.png" link="/"/>
+                    <MenuItem icon="/assets/order.png" link="/orders"/>
+                    <MenuItem icon="/assets/profile.png" link="/profile"/>
+                </Menu>
+                <PageBody>    
+                    <Switch>
+                        <Route exact path="/">
+                            <HomeScreen />
+                        </Route>
+                        <Route path="/orders">
+                            <div>TELA DE PEDIDOS</div>
+                        </Route>
+                        <Route path="/profile">
+                            <div>TELA DE PERFIL</div>
+                        </Route>
+                        <Route path="/tela2/:nome">
+                            <Tela2Screen />
+                        </Route>
+                    </Switch>
+                </PageBody>
+            </Container>
+        </BrowserRouter>
+    );
 }
-
-export default App;
